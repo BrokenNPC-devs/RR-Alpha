2c17da1f75d14ff8c814ef547e2a0b5ecc931e2d
Ismail Siddiqui <xismailsiddiquix@gmail.com>
2015-10-18 21:34:32 -0500

    Fixed platform dropping mechanic and tweaked jumping mechanic

--- Movement.cs
+++ Movement.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-	//public float walkSpeed = 5f;      //tweakable
-	public float maxSpeed = 10f;       //tweakable
+	public float walkSpeed = 5f;      //tweakable
+	public float runSpeed = 20;       //tweakable
+	public float jumpSpeed = 55f;     //tweakable
 	public LayerMask whatIsGround;
 	public Transform groundCheck;
-	public float jumpShortSpeed = 20f;   //tweakable
-	public float jumpSpeed = 55f;		//tweakable
 
 	bool facingRight = true;
 	bool grounded = false;
-	float groundRadius = 0.2f;
 	bool jump = false;
 	bool jumpCancel = false;
+	float groundRadius = 0.2f;
+	float dropTime = 0f;
 	Rigidbody2D rb;
+
+
+
 
 	void Start () {
 	
@@ -23,69 +26,55 @@ public class Movement : MonoBehaviour {
 	}
 
 	void Update () {
-		
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround); // ground
 
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround); // Find Ground
+		gameObject.layer = 8;
+		//***********************************Platform Mechanic****************************************************//
+		if (Input.GetAxis("Vertical") < 0 && Input.GetButton ("Jump") && grounded) {
+			jump = false;
+			Physics2D.IgnoreLayerCollision(8, 9);
+			dropTime = Time.time;
+		}
+		if (Time.time - dropTime > 0f && Time.time - dropTime >= 0.15f) {
+			Physics2D.IgnoreLayerCollision (8, 9, false);
+			dropTime = 0f;
+		}
+		//***********************************Platform Mechanic****************************************************//
+
+		
 		//***********************************Jump Mechanic****************************************************//
-		if (Input.GetButtonDown("Jump") && grounded)   // Player starts pressing the button
+		if (Input.GetButtonDown ("Jump") && grounded && Input.GetAxisRaw("Vertical") == 0)  // Player starts pressing the button
 			jump = true;
-		if (Input.GetButtonUp("Jump") && !grounded)     // Player stops pressing the button
+		if (Input.GetButtonUp ("Jump") && !grounded)   // Player stops pressing the button
 			jumpCancel = true;
 		//***********************************Jump Mechanic****************************************************//
-
-		//***********************************Platform Mechanic****************************************************//
-		if (grounded && Input.GetButtonDown("Jump")) { // Change to No Platform Collision layer
-			gameObject.layer = 10;
-		} else if (!Input.GetButton ("Jump")) { // Change to Player layer
-			gameObject.layer = 8;
-		} 
-
-		if (grounded && Input.GetAxis("Vertical") < 0) {
-			gameObject.layer = 10;
-			Physics2D.gravity = new Vector2(0f, -300f); //change gravity to x2 normal gravity
-		}
-		//***********************************Platform Mechanic****************************************************//
-	
 	}
-
-
+	
 	void FixedUpdate () {
-	
-		//***********************************Horizontal Movement****************************************************//
+		
 		float move = Input.GetAxisRaw ("Horizontal");
-		rb.velocity = new Vector2 (move * maxSpeed, rb.velocity.y);
-
-		if (move > 0 && !facingRight) {
+		if (move > 0 && !facingRight)
 			Flip ();
-		}
-
-		if (move < 0 && facingRight) {
+		if (move < 0 && facingRight) 
 			Flip ();
-		}
-		//***********************************Horizontal Movement****************************************************//
-	
-
+		rb.velocity = new Vector2 (move * runSpeed, rb.velocity.y);
 
 		//***********************************Jump Mechanic****************************************************//
 		if (jump) {
-			rb.gravityScale = 0.3f;
 			rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
 			jump = false;
 		}
 
 		if (jumpCancel) {
-			if (rb.velocity.y > jumpShortSpeed) { 
-				rb.gravityScale = 0.3f;
-				rb.velocity = new Vector2(rb.velocity.x, jumpShortSpeed);
+			if (rb.velocity.y > 0) { 
+				rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y / 2f);      // Change upward velocity by a factor of 1/x, x is tweakable
 			}
 			jumpCancel = false;
 		}
-		rb.gravityScale = 1f;
-		Physics2D.gravity = new Vector2 (0, -150); //tweakable
 		//***********************************Jump Mechanic****************************************************//
 	
 	}
-
+	
 	void Flip() {
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
