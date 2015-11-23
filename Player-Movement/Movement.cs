a6f47d1be752a35f3481d73312c7d7daaa60d286
Ismail Siddiqui <xismailsiddiquix@gmail.com>
2015-11-23 00:38:58 -0600

    Changed Crouch & PlatformD

--- Player-Movement/Movement.cs
+++ Player-Movement/Movement.cs
@@ -7,7 +7,9 @@ public class Movement : MonoBehaviour {
 	public LayerMask platform;
 	public Transform groundCheck;
 	public Transform platformCheck;
-	
+
+	bool startCrouch = false;
+	bool isCrouching = false;
 	bool facingRight = true;
 	bool jump = false;
 	bool jumpCancel = false;
@@ -15,9 +17,7 @@ public class Movement : MonoBehaviour {
 	bool isGrounded = false;
 	bool isPlatformed = false;
 	
-	float dropTime = 0f;
-	float dropDiff = 0.15f;     //tweakable
-	float tapTime = 0f; 
+	float tapTime;
 	float tapDiff = 0.2f;       //tweakable
 	float walkSpeed = 10f;      //tweakable
 	float runSpeed = 20f;       //tweakable
@@ -25,9 +25,11 @@ public class Movement : MonoBehaviour {
 	float fixJumpSpeed = 55f; //tweakable
 	float groundRadius = 0.75f; //tweakable
 	float platformRadius = 0.75f; //tweatkable
-	
-	Rigidbody2D rb;
+	float crouchTime;
 
+
+
+	Rigidbody2D rb;
 
 	void Flip() 
 	{
@@ -36,7 +38,7 @@ public class Movement : MonoBehaviour {
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
-
+	
 	void Walk() 
 	{	
 		float move = Input.GetAxisRaw("Horizontal");
@@ -44,11 +46,11 @@ public class Movement : MonoBehaviour {
 			Flip ();
 		if (move < 0 && facingRight)
 			Flip ();
-		if (Input.GetAxisRaw ("Vertical") < 0f)
+		if (Input.GetAxisRaw ("Vertical") < 0f && (isGrounded || isPlatformed))
 			move = 0;
 		rb.velocity = new Vector2 (move * walkSpeed, rb.velocity.y);
 	}
-
+	
 	void Run() 
 	{	
 		float move = Input.GetAxisRaw("Horizontal");
@@ -56,15 +58,17 @@ public class Movement : MonoBehaviour {
 			Flip ();
 		if (move < 0 && facingRight)
 			Flip ();
-		if (Input.GetAxisRaw ("Vertical") < 0f) 
+		if (Input.GetAxisRaw ("Vertical") < 0f && (isGrounded || isPlatformed)) 
 		{
 			move = 0;
 			doubleTap = false;
 			Walk ();
 		}
-		rb.velocity = new Vector2 (move * runSpeed, rb.velocity.y);
-		if (!Input.GetButton ("Horizontal")) //if not pressing horizontal movement button, stop running
+		if (Input.GetButtonDown ("Horizontal")) 
+		{
 			doubleTap = false;
+			Walk();
+		}
 		if(!isGrounded && !isPlatformed && (rb.velocity.x == (move * runSpeed))) //if your velocity in the x direction in the air is equal to the running velocity
 		{ 
 			if(Input.GetButtonDown("Horizontal"))  //if you press the horizontal movement button
@@ -73,8 +77,29 @@ public class Movement : MonoBehaviour {
 				Walk (); //change to walking speed
 			}
 		}
+		rb.velocity = new Vector2 (move * runSpeed, rb.velocity.y);
 	}
 
+	void isJumping(bool jumping)
+	{
+		if (jumping) 
+		{
+			if (isCrouching || startCrouch)
+				jumpSpeed = 0f;
+			else if(!isCrouching)
+				jumpSpeed = fixJumpSpeed;
+			rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+			jump = false;
+		}
+
+		if (!jumping) 
+		{
+			if (rb.velocity.y > 0) 
+				rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y / 3f);      // Change upward velocity by a factor of 1/x, x is tweakable
+			jumpCancel = false;
+		}
+	}
+	
 	void Start () 
 	{	
 		rb = GetComponent<Rigidbody2D> ();
@@ -82,31 +107,17 @@ public class Movement : MonoBehaviour {
 	
 	void Update () 
 	{
-		
 		isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, ground); //check if on the ground
 		isPlatformed = Physics2D.OverlapCircle (platformCheck.position, platformRadius, platform); //check if on a platform
 		gameObject.layer = 8;
 
-		if (Input.GetAxis ("Vertical") < 0 && Input.GetButtonDown ("Jump") && isPlatformed) 
-		{
-			jump = false;
-			Physics2D.IgnoreLayerCollision (8, 9); //ignore collision between player and platform 
-			dropTime = Time.time; //start a timer
-		}
-		if (Time.time - dropTime > 0f && Time.time - dropTime >= dropDiff) //if x amount of time has passed, reset the "ignore ollision" 
-		{ 
-			Physics2D.IgnoreLayerCollision (8, 9, false);
-			dropTime = 0f;
-		}
-
 		if (Input.GetButtonDown ("Jump") && isGrounded)  // Player starts pressing the jump button
 			jump = true;
-		else if (Input.GetButtonDown ("Jump") && isPlatformed && Input.GetAxis("Vertical") == 0f)
+		else if (Input.GetButtonDown ("Jump") && isPlatformed && !isCrouching)
 			jump = true;
 		if (Input.GetButtonUp ("Jump") && !isGrounded && !isPlatformed)   // Player stops pressing the button
 			jumpCancel = true;
 
-				
 		if (Input.GetButtonDown ("Horizontal") && (isGrounded || isPlatformed)) 
 		{
 			if(Time.time - tapTime > 0f && Time.time - tapTime < tapDiff) //player doublee taps button
@@ -116,31 +127,44 @@ public class Movement : MonoBehaviour {
 			tapTime = Time.time;
 		} 
 
+		if (Input.GetAxisRaw("Vertical") == -1f && !startCrouch) 
+		{
+			startCrouch = true;
+			crouchTime = Time.realtimeSinceStartup;
+			//put crouch animation here
+		}
+		if(startCrouch)
+		{
+			if(Time.realtimeSinceStartup - crouchTime >= 0.25f)
+			{
+				isCrouching = true;
+			}
+		}
+		if (isCrouching) 
+		{
+			startCrouch = false;
+		}
+		if (Input.GetAxisRaw ("Vertical") != -1f)
+			isCrouching = false;
 	}
 	
 	void FixedUpdate () 
 	{
 		if (jump) 
-		{
-			if (Input.GetAxisRaw ("Vertical") < 0f)
-				jumpSpeed = 0f;
-			else
-				jumpSpeed = fixJumpSpeed;
-			rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-			jump = false;
-		}
-		
+			isJumping (true);
 		if (jumpCancel) 
-		{
-			if (rb.velocity.y > 0) 
-				rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y / 2f);      // Change upward velocity by a factor of 1/x, x is tweakable
-			jumpCancel = false;
-		}
-
-
+			isJumping (false);
+		
 		if (doubleTap) // if player double tapped, run
 			Run ();
 		if (!doubleTap) // if not, walk
 			Walk ();
+
+		if (isCrouching && Input.GetButton ("Jump") && isPlatformed && rb.velocity.y == 0f) 
+			Physics2D.IgnoreLayerCollision (8, 9);
+		if (!isCrouching || !Input.GetButton("Jump"))
+			Physics2D.IgnoreLayerCollision (8, 9, false);
+		if (rb.velocity.y < -15f && isPlatformed) 
+			Physics2D.IgnoreLayerCollision(8,9,false);
 	}
 }
